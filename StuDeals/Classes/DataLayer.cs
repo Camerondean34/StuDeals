689d3178ee3d3eabf08b71a9a027a6f7848f0182
Cameron Johnson <C.D.JOHNSON-2021@hull.ac.uk>
2023-03-23 20:10:03 +0000

    Implemented more methods in DataLayer

--- StuDeals/Classes/DataLayer.cs
+++ StuDeals/Classes/DataLayer.cs
@@ -5,7 +5,7 @@ namespace StuDeals.Classes
     public class DataLayer
     {
         private static DataLayer? _Instance;
-        private const string _Source = @"Data\\Employees.db";
+        private const string _Source = "Data\\Employees.db";
         private SQLiteConnectionStringBuilder connectionStringBuilder;
 
         public static DataLayer Instance
@@ -20,29 +20,76 @@ namespace StuDeals.Classes
             }
         }
 
-        DataLayer()
+        private DataLayer()
         {
             connectionStringBuilder = new SQLiteConnectionStringBuilder();
             connectionStringBuilder.DataSource = _Source;
         }
 
-        public static Venue[] GetVenues()
+        private string[][] SelectAll(string pTableName, string pCondition = "")
         {
-            List<Venue> result = new List<Venue>();
+            List<string[]> result = new List<string[]>();
             using (SQLiteConnection connection = new SQLiteConnection())
             {
                 connection.Open();
                 SQLiteCommand getVenuesCommand = connection.CreateCommand();
-                getVenuesCommand.CommandText = "SELECT * FROM 'Venues';";
+                getVenuesCommand.CommandText = $"SELECT * FROM '{pTableName}' {pCondition};";
                 using (var reader = getVenuesCommand.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        result.Add(new Venue(int.Parse((string)reader[0]), (string)reader[1], (string)reader[2], (string)reader[3], (string)reader[4]));
+                        string[] current = new string[reader.VisibleFieldCount];
+                        for (int index = 0; index < reader.FieldCount; ++index)
+                        {
+                            current[index] = (string)reader.GetValue(index);
+                        }
+                        result.Add(current);
                     }
                 }
             }
             return result.ToArray();
+        }
+
+        public Venue[] GetVenues()
+        {
+            string[][] venueFields = SelectAll("Venues");
+            Venue[] result = new Venue[venueFields.Length];
+            for (int index = 0; index < venueFields.Length; ++index)
+            {
+                int ID = int.Parse(venueFields[index][0]);
+                result[index] = new Venue(ID, venueFields[index][1], venueFields[index][2], venueFields[index][3], venueFields[index][4]);
+                string[][] tags = SelectAll("Tags", $"WHERE 'ID' = '{ID}'");
+                foreach (string[] tag in tags) result[index].AddTag(tag[0]);
+            }
+            return result;
+        }
+
+        public Deal[] GetDeals()
+        {
+            string[][] dealFields = SelectAll("Deals");
+            Deal[] result = new Deal[dealFields.Length];
+            for (int index = 0; index < dealFields.Length; ++index)
+            {
+                result[index] = new Deal(int.Parse(dealFields[index][0]), dealFields[index][1], dealFields[index][2], dealFields[index][3]);
+            }
+            return result;
+        }
+
+        private void Insert(string pTableCols, string pValues)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection())
+            {
+                connection.Open();
+                SQLiteCommand getVenuesCommand = connection.CreateCommand();
+                getVenuesCommand.CommandText = $"INSERT INTO {pTableCols} VALUES {pValues};";
+                getVenuesCommand.ExecuteNonQuery();
+            }
+        }
+
+        public void InsertVenue(Venue venue)
+        {
+            Insert("'Venues'", $"({""},{""},{""},{""},{""})");
+            //TODO create properties in Venue class
         }
     }
 }
