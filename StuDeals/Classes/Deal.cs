15b19633125de08ebaa8a64bf6a50d4414c1f869
Camerondean34 <C.D.JOHNSON-2021@hull.ac.uk>
2023-03-23 16:49:58 +0000

    Outlined initial Deal and Venue classes

---
+++ StuDeals/Classes/Deal.cs
@@ -0,0 +1,24 @@
+﻿namespace StuDeals.Classes
+{
+    public class Deal
+    {
+        private string _ID;
+        private string _Name;
+        private string _Description;
+        private string _Image;
+
+        public Deal(string pID, string pName, string pDescription, string pImage)
+        {
+            _ID = pID;
+            _Name = pName;
+            _Description = pDescription;
+            _Image = pImage;
+        }
+
+
+        public override string ToString()
+        {
+            return _Name;
+        }
+    }
+}
---
+++ StuDeals/Classes/Venue.cs
@@ -0,0 +1,32 @@
+﻿namespace StuDeals.Classes
+{
+    public class Venue
+    {
+        private string _ID;
+        private string _Name;
+        private string _Description;
+        private string _Location;
+        private string _Image;
+        private List<string> _Tags;
+
+        public Venue(string pID, string pName, string pDescription, string pLocation, string pImage)
+        {
+            _ID = pID;
+            _Name = pName;
+            _Description = pDescription;
+            _Location = pLocation;
+            _Image = pImage;
+            _Tags = new List<string>();
+        }
+
+        public void AddTag(string pTag)
+        {
+            _Tags.Add(pTag);
+        }
+
+        public override string ToString()
+        {
+            return _Name;
+        }
+    }
+}
