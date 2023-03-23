2fcd1b620a3fbd3e003849304da2b64acd143d0f
Camerondean34 <C.D.JOHNSON-2021@hull.ac.uk>
2023-03-23 17:14:58 +0000

    Outlined initial DataLayer class

---
+++ StuDeals/Classes/DataLayer.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.SQLite;
+
+namespace StuDeals.Classes
+{
+    public class DataLayer
+    {
+        private static DataLayer? _Instance;
+        private const string _Source = @"Data\\Employees.db";
+        private SQLiteConnectionStringBuilder connectionStringBuilder;
+
+        public static DataLayer Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new DataLayer();
+                }
+                return _Instance;
+            }
+        }
+
+        DataLayer()
+        {
+            connectionStringBuilder = new SQLiteConnectionStringBuilder();
+            connectionStringBuilder.DataSource = _Source;
+        }
+
+        public static Venue[] GetVenues()
+        {
+            List<Venue> result = new List<Venue>();
+            using (SQLiteConnection connection = new SQLiteConnection())
+            {
+                connection.Open();
+                SQLiteCommand getVenuesCommand = connection.CreateCommand();
+                getVenuesCommand.CommandText = "SELECT * FROM 'Venues';";
+                using (var reader = getVenuesCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        result.Add(new Venue(int.Parse((string)reader[0]), (string)reader[1], (string)reader[2], (string)reader[3], (string)reader[4]));
+                    }
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
